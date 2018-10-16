ef638efd7858ad89d8176b9ef3c3d9fb0c8a7ab7
Vyper248 <vyper375@hotmail.com>
2018-10-16 13:34:04 +0100

    switched robots to Redux, added redux-thunk middleware

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,23 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAIL
+ } from './constants.js';
 
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
     payload: text
 });
+
+export const requestRobots = () => (dispatch) => {
+    dispatch({ type: REQUEST_ROBOTS_PENDING });
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(robots => {
+            //added check to make sure returned robots is an array that has something in it
+            if (Array.isArray(robots) && robots.length > 0) dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: robots })
+            else dispatch({ type: REQUEST_ROBOTS_FAIL, payload: 'No Robots Returned'});
+        })
+        .catch(err => dispatch({ type: REQUEST_ROBOTS_FAIL, payload: err.message }));
+};
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,33 +6,31 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     };
 }
     
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     };
 };
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-        };
-    }
     
     render(){
-        const { robots } = this.state;
-        const {searchField, onSearchChange } = this.props;
+        const {searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
-        if (robots.length === 0){
+
+        if (isPending){
             return <h1 id='heading'>Loading Robots!!</h1>
         } else {
             return (
@@ -50,10 +48,7 @@ class App extends Component {
     }
     
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(robots => this.setState({ robots: robots }))
-            .catch(err => console.log(err.message));
+        this.props.onRequestRobots();
     }
 };
 
